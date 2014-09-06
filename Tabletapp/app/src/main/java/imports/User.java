2174d538a7a30ab46556c4c29845d0669b67a8f9
FLambda <hg@flambda.de>
2014-09-06 11:13:09 +0200

    Added a User class.

---
+++ Tabletapp/app/src/main/java/imports/User.java
@@ -0,0 +1,52 @@
+package imports;
+
+public class User {
+    private String lastname;
+    private String firstname;
+    private String user_email;
+    private String pw_hash;
+
+
+    public User(String firstname, String lastname, String user_email, String pw_hash){
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.pw_hash = pw_hash;
+        this.user_email = user_email;
+    }
+
+    public String display(Boolean lastname_first, String separator){
+        if(this.lastname != null && this.firstname != null){
+            if(lastname_first){
+                return this.lastname + separator + this.firstname;
+            }else{
+                return this.firstname + separator + this.lastname;
+            }
+        }else{
+            return this.user_email;
+        }
+    }
+
+    public String display(){
+        return this.display(Boolean.FALSE, " ");
+    }
+
+    public void setName(String firstname, String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public void setLogin(String user_email, String pw_hash){
+        this.user_email = user_email;
+        this.pw_hash = pw_hash;
+    }
+
+    public String getUser_email(){
+        return this.user_email;
+    }
+
+    public String getPw_hash(){
+        return this.pw_hash;
+    }
+
+
+}
