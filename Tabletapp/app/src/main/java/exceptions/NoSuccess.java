3da92db7549f0fd159202fc240a62e8827364b02
Hawk Anonymous <acc-git@flambda.de>
2014-10-11 15:59:10 +0200

    Some clean up, cannot go on right now as DB was tinkered with and is not working.

---
+++ Tabletapp/app/src/main/java/exceptions/NoSuccess.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+/**
+ * Created by p91 on 11.10.14.
+ */
+public class NoSuccess {
+}
---
+++ Tabletapp/app/src/main/java/scon/Entry_id_timestamp.java
@@ -0,0 +1,7 @@
+package scon;
+
+/**
+ * Created by p91 on 11.10.14.
+ */
+public class Entry_id_timestamp {
+}
